e3cf47cbcd4851788945971d553f3575d25d2c7d
smartfridge <37928912+smartfrigde@users.noreply.github.com>
2021-05-13 16:37:19 +0200

    Add uninstall option and tweak UI a bit

--- inject.js
+++ inject.js
@@ -22,3 +22,25 @@ document.getElementById("install").onclick = function () {
     document.getElementById("status").innerHTML = "Injected!";
   });
 };
+document.getElementById("uninstall").onclick = function () {
+  document.getElementById("status").innerHTML = "Uninstalling!";
+  var fs = require("fs");
+  var branch = document.getElementById("channel").value;
+  console.log(branch);
+  var basepath =
+    process.env.APPDATA ||
+    (process.platform == "darwin"
+      ? process.env.HOME + "/Library/Preferences"
+      : process.env.HOME + "/.local/share");
+  var path = basepath + `\\${branch}`;
+  console.log(`${path}\\settings.json`);
+  fs.readFile(`${path}\\settings.json`, function (err, data) {
+    console.log(data);
+    var json = JSON.parse(data); //https://github.com/Goose-Nest/GooseUpdate/blob/main/src/apiV1/dashboard/template.html
+    json["UPDATE_ENDPOINT"] = null;
+    json["NEW_UPDATE_ENDPOINT"] = null;
+    
+    fs.writeFileSync(`${path}\\settings.json`, JSON.stringify(json));
+    document.getElementById("status").innerHTML = "Uninstalled!";
+  });
+};
